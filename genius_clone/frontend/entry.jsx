709aec0d445954c86d558c999c9eb656121e8098
SHP <skylarprill@gmail.com>
2018-12-11 16:23:33 -0800

    Write thunk action creators for session and users

--- genius_clone/frontend/entry.jsx
+++ genius_clone/frontend/entry.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//DELETE TEST MATERIAL STARTING
+import { signup, login, logout } from "./util/session_api_util";
+//ENDING HERE
+
 document.addEventListener('DOMContentLoaded', () => {
+
+  // DELETE TEST MATERIAL STARTING 
+  window.signup = signup;
+  window.login = login;
+  window.logout = logout;
+  // ENDING HERE
+
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Genius</h1>, root);
 });
