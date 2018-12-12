271424145e5aa27938ac7cfae8407447185a81c4
SHP <skylarprill@gmail.com>
2018-12-11 18:02:51 -0800

    Finish reducers and configureStore, start components

--- genius_clone/frontend/entry.jsx
+++ genius_clone/frontend/entry.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from "./store/store";
 
 //DELETE TEST MATERIAL STARTING
 import { signup, login, logout } from "./util/session_api_util";
 //ENDING HERE
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
 
   // DELETE TEST MATERIAL STARTING 
   window.signup = signup;
   window.login = login;
   window.logout = logout;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   // ENDING HERE
 
   const root = document.getElementById('root');
---
+++ genius_clone/frontend/reducers/users_reducer.js
@@ -0,0 +1,16 @@
+import merge from "lodash/merge";
+import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
+
+export const usersReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      const newUser = { [action.user.id]: action.user }
+      const newState = merge( {}, state, newUser );
+      return newState;
+    default:
+      return state
+  }
+}
+
+export default usersReducer;
