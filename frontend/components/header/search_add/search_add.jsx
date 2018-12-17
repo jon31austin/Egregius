77e738417d6bef6a52cb94b9b1b248c17e2582f5
SHP <skylarprill@gmail.com>
2018-12-16 20:58:24 -0800

    Fix minor styling on main header for alignment

--- frontend/components/header/search_add/search_add.jsx
+++ frontend/components/header/search_add/search_add.jsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 
 const SearchAdd = () => {
   return (
-    <div className="greeting-action">
+    <div className="left-nav">
+      <div className="add-song-link">
         <Link to="/tracks/new">Submit New Song</Link>
+      </div>
     </div>
   )
 };
