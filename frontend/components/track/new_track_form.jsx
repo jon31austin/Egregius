36ec33bb4ffd283e02c089e6ab1b187076f9b3d0
SHP <skylarprill@gmail.com>
2019-01-25 17:41:44 -0800

    Style track create page and fix layout with album cover button

--- frontend/components/track/new_track_form.jsx
+++ frontend/components/track/new_track_form.jsx
@@ -62,41 +62,50 @@ class NewTrackForm extends React.Component {
 
     return (
 
-      <form className="new-song-form" onSubmit={this.handleSubmit}>
+      <div className="create-container">
         
-        <div className="new-song-lyrics">
-          <h2>Lyrics</h2>
-          <textarea onChange={this.update("lyrics")} className="new-song-textarea" value={this.state.lyrics}></textarea>
-        </div>
+        <form className="new-song-form" onSubmit={this.handleSubmit}>
+          
+          <div className="new-song-lyrics">
+            <h2>Lyrics</h2>
+            <textarea onChange={this.update("lyrics")} className="new-song-textarea" value={this.state.lyrics}></textarea>
+          </div>
 
-        <div className="new-song-info">
-          <div>
-            <h3>Track Name</h3>
-            <input onChange={this.update("title")} type="text" value={this.state.title}/>
-          </div>
-          <div>
-            <h3>Artist</h3>
-            <input onChange={this.update("artist")} type="text" value={this.state.artist}/>
-          </div>
-          <div>
-            <h3>Album</h3>
-            <input onChange={this.update("album")} type="text" value={this.state.album} />
-            <input type="file" 
+          <div className="new-song-info">
+            <div>
+              <h3>Track Name</h3>
+              <input onChange={this.update("title")} type="text" value={this.state.title}/>
+            </div>
+            <div>
+              <h3>Artist</h3>
+              <input onChange={this.update("artist")} type="text" value={this.state.artist}/>
+            </div>
+            <div>
+              <h3>Album</h3>
+              <input onChange={this.update("album")} type="text" value={this.state.album} />
+            </div>
+
+            <div className="album-cover">
+              <h3>Album Cover</h3>
+              <input type="file"
                 onChange={this.handleFile.bind(this)} />
-          </div>
-          <div>
-            <h3>Year Released</h3>
-            <input onChange={this.update("year")} type="text" value={this.state.year} />
-          </div>
+            </div>
 
-          <div className="track-submit-bucket">
-            <input type="submit" className="new-song-submit" value="Create New Song" />
+            
+            <div>
+              <h3>Year Released</h3>
+              <input onChange={this.update("year")} type="text" value={this.state.year} />
+            </div>
+
+            <div className="track-submit-bucket">
+              <input type="submit" className="new-song-submit" value="Create New Song" />
+            </div>
+
+            <TrackFormErrorModal errors={this.props.errors} />
           </div>
+        </form>
 
-          <TrackFormErrorModal errors={this.props.errors} />
-        </div>
-
-      </form>
+      </div>
 
     )
   }
