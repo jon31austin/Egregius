9d068b023142e79916b238e42fe2bb348aaae92e
SHP <skylarprill@gmail.com>
2019-01-28 15:43:58 -0800

    Add styling to news box on home page

--- frontend/components/home/news.jsx
+++ frontend/components/home/news.jsx
@@ -3,25 +3,28 @@ import React from "react";
 const News = () => {
   return (
     <div className="news">
-      <div className="featured-stories">
-        <h1>Featured Stories</h1>
-        <div className="news-test">
-          <img src="/assets/news/news1.jpg" />
-          <div>
-            TWO IMAGE
-            THREE IMAGE
-          </div>
-        </div>
+
+      <div className="news-headers">
+        <div className="featured-stories"><h1>Featured Stories</h1></div>
+        <div className="latest-news"><h1>Latest News</h1></div>
       </div>
 
-      <div className="latest-news">
-        <h1>Latest</h1>
-        <div>
-          UL OF STUFF
+      <div className="news-content">
+        <div className="news-left-pic"></div>
+
+        <div className="news-right">
+          <div className="news-right-top"></div>
+          <div className="news-right-bottom"></div>
         </div>
+
+        <div className="latest-news"></div>
       </div>
+     
     </div>
   )
 };
 
 export default News;
+
+{/* <img src="/assets/news/news2.jpg" />
+  <img src="/assets/news/news3.jpg" /> */}
