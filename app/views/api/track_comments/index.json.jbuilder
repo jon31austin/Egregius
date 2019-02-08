8ff98fc4d6989ed79d4e6be4cd0ee6fcf40b08d9
SHP <skylarprill@gmail.com>
2019-02-07 21:30:51 -0800

    Style track comments box and filter comments in redux state by track id

--- app/views/api/track_comments/index.json.jbuilder
+++ app/views/api/track_comments/index.json.jbuilder
@@ -1,6 +1,6 @@
 @track_comments.each do |comment|
   json.set! comment.id do 
-    json.extract! comment, :id, :body
+    json.extract! comment, :id, :body, :track_id
     json.username comment.user.username
   end
 end
--- frontend/components/track/comments/track_comments.jsx
+++ frontend/components/track/comments/track_comments.jsx
@@ -3,30 +3,43 @@ import React from "react";
 class TrackComments extends React.Component {
   constructor(props){
     super(props)
+
+    this.formatTrackComments = this.formatTrackComments.bind(this)
   }
 
   componentDidMount() {
     this.props.fetchComments(this.props.trackId);
   }
 
-  formatTrackComments() {
-    const coms = this.props.comments.map(com => (
-      <li>
+  formatTrackComments(trackId) {
+    const coms = this.props.comments.filter(com => {
+      debugger;
+      return (com.track_id === trackId)
+    }).map(com => (
+      <li className="track-comment">
         <div>
-          <h1>{com.username} said:</h1>
+          <h1 className="track-comment-username">{com.username}</h1>
           <p>{com.body}</p>
         </div>
       </li>
     ))
     
+    if (coms.length === 0) {
+      return (
+        <div className="track-comment">
+          <h1>Be the first to leave a comment!</h1>
+        </div>
+      )
+    }
     return coms;
   }
 
   render() {
     return(
-      <div className="track-comments">
+      <div className="track-comments-box">
+        <input className="track-comment-input" type="text" placeholder="Add a comment"></input>
         <ul>
-          {this.formatTrackComments()}
+          {this.formatTrackComments(this.props.trackId)}
         </ul>
       </div>
     )
