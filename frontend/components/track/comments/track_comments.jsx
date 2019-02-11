34212883cc2c9b07c03ec5e89cca9a3657bbc502
SHP <skylarprill@gmail.com>
2019-02-10 21:33:51 -0800

    Write track comment submit functionality

--- frontend/components/track/comments/track_comments.jsx
+++ frontend/components/track/comments/track_comments.jsx
@@ -5,7 +5,8 @@ class TrackComments extends React.Component {
     super(props)
 
     this.state = ({ 
-      sortBy: "date"
+      sortBy: "date",
+      comment: ""
     })
 
     this.formatTrackComments = this.formatTrackComments.bind(this)
@@ -27,7 +28,7 @@ class TrackComments extends React.Component {
     })
     
     coms = coms.sort(this.compare)
-    debugger;
+  
     coms = coms.map(com => (
       <li className="track-comment">
         <div>
@@ -51,10 +52,38 @@ class TrackComments extends React.Component {
     return coms;
   }
 
+  handleInput(e) {
+    this.setState({
+      comment: e.currentTarget.value
+    })
+  };
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.props.submitComment({
+      body: this.state.comment,
+      track_id: this.props.trackId,
+      user_id: this.props.currentUser
+    })
+  }
+
   render() {
     return(
       <div className="track-comments-box">
-        <input className="track-comment-input" type="text" placeholder="Add a comment"></input>
+
+        <form onSubmit={this.handleSubmit}>
+          <input
+            onChange={this.handleInput}
+            className="track-comment-input"
+            type="text"
+            value={this.state.comment} placeholder="Add a comment">
+          </input>
+
+          {this.props.loggedIn ? <input className="submit" type="submit" />
+            : <h1>Please sign in to submit a comment!</h1>}
+        </form>
+        
         <ul>
           {this.formatTrackComments(this.props.trackId)}
         </ul>
--- frontend/components/track/comments/track_comments_container.jsx
+++ frontend/components/track/comments/track_comments_container.jsx
@@ -3,19 +3,25 @@ import { connect } from "react-redux";
 
 import TrackComments from "./track_comments";
 
-import { fetchTrackComments } from "../../../actions/track_comment_actions";
+import { 
+  fetchTrackComments,
+  createTrackComment
+ } from "../../../actions/track_comment_actions";
 
 
 const msp = (state, ownProps) => {
   let comments = Object.values(state.entities.trackComments)
   return {
-    comments
+    comments,
+    currentUser: state.session.id,
+    loggedIn: !!(state.session.id)
   }
 }
 
 const mdp = (dispatch) => {
   return {
-    fetchComments: (id) => dispatch(fetchTrackComments(id))
+    fetchComments: (id) => dispatch(fetchTrackComments(id)),
+    submitComment: (comment) => dispatch(createTrackComment(comment))
   }
 }
 
